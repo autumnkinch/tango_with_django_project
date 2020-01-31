3b0150539868706cdc24c767637d6d64db19e228
autumnkinch <60010623+autumnkinch@users.noreply.github.com>
2020-01-31 16:11:51 +0000

    End of Chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -6,10 +6,13 @@ Created on Thu Jan 30 20:33:11 2020
 """
 
 from django.urls import path
+from django.conf import settings
 from rango import views
+from django.conf.urls.static import static
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
