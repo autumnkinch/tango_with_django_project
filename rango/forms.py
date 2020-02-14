86dc0c0501696f55bd69dffe488bdcee0c6a1532
autumnkinch <60010623+autumnkinch@users.noreply.github.com>
2020-02-14 13:11:28 +0000

    ch7 exercise

--- rango/forms.py
+++ rango/forms.py
@@ -6,7 +6,7 @@ from rango.models import Page, Category
 # We could add these forms to views.py, but it makes sense to split them off into their own file.
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -16,10 +16,20 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=Page.MAX_LENGTH_TITLE, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=Page.MAX_LENGTH_URL, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
         exclude = ('category',)
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
+
--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    MAX_LENGTH = 128
+    
+    name = models.CharField(MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -18,10 +20,13 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
+    MAX_LENGTH_TITLE = 128
+    MAX_LENGTH_URL = 200
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(MAX_LENGTH_TITLE)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
 
     def __str__(self):
         return self.title
