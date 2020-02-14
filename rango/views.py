2e56fdd82fffdc68701187fb8c59d4d32b04ac54
autumnkinch <60010623+autumnkinch@users.noreply.github.com>
2020-02-14 00:44:07 +0000

    ch6

--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-   context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
    return render(request, 'rango/index.html', context=context_dict)
+
+    
 
 def about(request):
     return render(request, 'rango/about.html')
