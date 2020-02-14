96ac927f37c39dde7108b628aa56f5edb1c40de0
autumnkinch <60010623+autumnkinch@users.noreply.github.com>
2020-02-14 15:00:37 +0000

    login implementation

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,8 @@ from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm
 from rango.forms import UserProfileForm
+from django.contrib.auth import authenticate, login
+
 
 
 
@@ -114,5 +116,22 @@ def register(request):
         profile_form = UserProfileForm()
     
     return render(request, 'rango/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
-    
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
 
