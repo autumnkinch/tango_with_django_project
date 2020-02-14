e34226beef3acb524e5622de978b06b68d19c977
autumnkinch <60010623+autumnkinch@users.noreply.github.com>
2020-02-14 14:48:51 +0000

    REGISTRY TIME

--- rango/forms.py
+++ rango/forms.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
-# We could add these forms to views.py, but it makes sense to split them off into their own file.
+
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.MAX_LENGTH, help_text="Please enter the category name.")
@@ -32,4 +34,16 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
         return cleaned_data
+    
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+        
+
 
